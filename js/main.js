e50cf343509a04108400938e538f9297afeb1135
nney09 <neyyy.09@gmail.com>
2020-07-19 15:33:16 -0400

    Portafolio

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    "use strict";
+
+    //------- Filter  js --------//  
+
+      $('.filters ul li').click(function(){
+        $('.filters ul li').removeClass('active');
+        $(this).addClass('active');
+        
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+          filter: data
+        })
+      });
+
+
+      if(document.getElementById("portfolio")){
+            var $grid = $(".grid").isotope({
+              itemSelector: ".all",
+              percentPosition: true,
+              masonry: {
+                columnWidth: ".all"
+              }
+            })
+      };
+
+});
