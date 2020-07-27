5f368d63cb493ee71df400903637b9ef372a8f45
nney09 <neyyy.09@gmail.com>
2020-07-27 16:23:12 -0400

    edition and thumbnails

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
-$(document).ready(function() {
-    "use strict";
+$(document).ready(function () {
+  "use strict";
 
-    //------- Filter  js --------//  
+  //------- Filter  js --------//
 
-      $('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-          filter: data
-        })
-      });
+  $(".filters ul li").click(function () {
+    $(".filters ul li").removeClass("active");
+    $(this).addClass("active");
 
+    var data = $(this).attr("data-filter");
+    $grid.isotope({
+      filter: data,
+    });
+  });
 
-      if(document.getElementById("portfolio")){
-            var $grid = $(".grid").isotope({
-              itemSelector: ".all",
-              percentPosition: true,
-              masonry: {
-                columnWidth: ".all"
-              }
-            })
-      };
+  if (document.getElementById("portfolio")) {
+    var $grid = $(".grid").isotope({
+      itemSelector: ".all",
+      percentPosition: true,
+      masonry: {
+        columnWidth: ".all",
+      },
+    });
+  }
 
+  $(".img-pop-up").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
 });
+
+setTimeout(() => {
+  $(".filters ul li:nth-child(1)").trigger("click");
+}, 500);
